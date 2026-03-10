c738c6f96bb367acfaed7dbf92702d9763ab1bc1
hritikpatil405 <hritikpatil405@gmail.com>
2026-03-10 21:32:17 +0530

    Fix file path formatting for model and encoder

--- salary_prediction_deployment.py
+++ salary_prediction_deployment.py
@@ -11,8 +11,8 @@ import streamlit as st
 import pandas as pd
 import joblib
 
-model = joblib.load('/salary_prediction_rfr_model.pkl')
-encoder = joblib.load('/label_encoder_Salary.pkl')
+model = joblib.load('salary_prediction_rfr_model.pkl')
+encoder = joblib.load('label_encoder_Salary.pkl')
 
 st.title('Salary Prediction model')
 
